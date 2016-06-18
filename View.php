6c51bbcba1f5a0805ccc1dee6acb827f3bba8fd5
hkm-mo <hkmcss@gmail.com>
2016-06-19 01:10:19 +0800

    Add defined('BASEPATH') OR exit('No direct script access allowed');

--- View.php
+++ View.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 include_once __DIR__ . '/View_menu.php';
 
--- View_menu.php
+++ View_menu.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class View_menu {
 	private $menu_data = array();
